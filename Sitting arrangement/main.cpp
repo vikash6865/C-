dd7e210fed5c9869a856b8e987a2a42209ebd883
jatin kumar <42140725+jamloya@users.noreply.github.com>
2018-10-09 18:53:59 +0530

    Update main.cpp

--- Sitting arrangement/main.cpp
+++ Sitting arrangement/main.cpp
@@ -37,51 +37,159 @@
 // Marking Scheme: Marks are awarded when all the testcases pass.
 // Allowed Languages: Bash, C, C++, C++14, Clojure, C#, D, Erlang, F#, Go, Groovy, Haskell, Java, Java 8, JavaScript(Rhino), JavaScript(Node.js), Julia, Kotlin, Lisp, Lisp (SBCL), Lua, Objective-C, OCaml, Octave, Pascal, Perl, PHP, Python, Python 3, R(RScript), Racket, Ruby, Rust, Scala, Swift, Swift-4.1, Visual Basic
 
-#include<iostream>
+#include <iostream>
+
+
 using namespace std;
 
-int main(){
-    int n,s,seat,opp;
-    string type;
-        cout<<"Enter number of test cases: \n";
-        cin>>n;
-        for(int i=0; i<n; ++i){
-            cout<<"Enter seat number: \n";
-            cin>>s;
-            seat=s%12;
-            if(seat==1 || seat==6 || seat==7 || seat==12){
-                type="WS";
-                if(seat==1)
-                    opp=s+11;
-                else if(seat==6)
-                    opp=s+1;
-                else if(seat==7)
-                    opp=s-1;
-                else
-                    opp=s-11;
-            }
-            else if(seat==2 || seat==5 || seat==8 || seat==11){
-                type="MS";
-                if(seat==2)
-                    opp=s+9;
-                else if(seat==5)
-                    opp=s+3;
-                else if(seat==8)
-                    opp=s-3;
-                else
-                    opp=s-9;
-            }
-            else{
-                type="AS";
-                if(seat==3)
-                    opp=s+7;
-                else if(seat==4)
-                    opp=s+5;
-                else if(seat==9)
-                    opp=s-5;
-                else
-                    opp=s-7;
-            }
-            cout<<"seat opposite to "<<s<<" is: "<<opp<<" and its type is: "<<type<<endl;
-            }
-        }
+
+int main() {
+
+	int d,n[100000],a,m,x;
+
+	cin>>a;
+
+for (int i=0;i<a;i++)
+
+{	cin>>n[i];
+
+	d=n[i]%6;
+	m=n[i]/6;
+	x=m%2;
+if (n[i]%12!=0 && (x==0 || n[i]%6==0))
+{
+
+switch (d)
+
+{
+
+case 0:
+
+	n[i]=n[i]+1;
+
+cout<<n[i]<<" WS"<<endl;
+
+break;
+
+
+case 1:
+
+
+	n[i]=n[i]+11;
+
+	cout<<n[i]<<" WS"<<endl;
+
+	break;
+
+	
+
+case 2:
+
+	n[i]=n[i]+9;
+
+	cout<<n[i]<<" MS"<<endl;
+
+	break;
+
+	
+
+case 3:
+
+	n[i]=n[i]+7;
+
+	cout<<n[i]<<" AS"<<endl;
+
+break;
+
+
+case 4:
+
+	n[i]=n[i]+5;
+
+	cout<<n[i]<<" AS"<<endl;
+
+	break;
+
+	
+
+case 5:
+
+	n[i]=n[i]+3;
+
+	cout<<n[i]<<" MS"<<endl;
+
+	break;}
+
+}
+
+else
+
+	{
+	switch (d)
+
+{
+
+case 0:
+
+	n[i]=n[i]-11;
+
+cout<<n[i]<<" WS"<<endl;
+
+ break;
+
+
+case 1:
+
+
+	n[i]=n[i]-1;
+
+	cout<<n[i]<<" WS"<<endl;
+
+	break;
+
+	
+
+case 2:
+
+	n[i]=n[i]-3;
+
+	cout<<n[i]<<" MS"<<endl;
+
+	break;
+
+	
+
+case 3:
+
+	n[i]=n[i]-5;
+
+	cout<<n[i]<<" AS"<<endl;
+
+break;
+
+
+case 4:
+
+	n[i]=n[i]-7;
+
+	cout<<n[i]<<" AS"<<endl;
+
+	break;
+
+	
+
+case 5:
+
+	n[i]=n[i]-9;
+
+	cout<<n[i]<<" MS"<<endl;
+
+	break;
+
+}
+
+}
+
+}
+
+	return 0;}
